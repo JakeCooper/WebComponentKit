c6fa01366c9a71e75bf5f8f97a3f1dc0822a5a11
Jake Cooper <jake.elijah.cooper@gmail.com>
2019-03-10 22:38:52 -0700

    Version 0.2.1

--- src/WebComponentKit.tsx
+++ src/WebComponentKit.tsx
@@ -1,7 +1,8 @@
 import Modal from 'core/Modal';
+import Button from 'core/Button';
+import Footer from 'core/Footer';
+import Section from 'core/Section';
+import Input from 'core/Input';
+import Slider from 'core/Slider';
 
-const WebComponentKit = {
-    Modal
-};
-
-export default WebComponentKit;
+export { Modal, Button, Footer, Input, Section, Slider };
